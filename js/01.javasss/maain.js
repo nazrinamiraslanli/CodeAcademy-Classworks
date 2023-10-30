30e1642843da24f8165a117ef8d6b3b0467b9665
nazrinamiraslanli <nazrinamiraslanli38@gmail.com>
2023-10-30 02:47:30 -0700

    task

---
+++ js/01.javasss/maain.js
@@ -0,0 +1,17 @@
+let firstName ="Nazrin"
+let lastName ="Amiraslanli"
+let fullName= firstName+ " " + lastName;
+
+console.log("fullName: ",fullName)
+
+let country="Azerbaijan"
+let job="student"
+let age="20"
+console.log("I am a "+job+". "+"I live in "+country+". "+"I am "+age+" years old.")
+
+
+
+
+
+
+
